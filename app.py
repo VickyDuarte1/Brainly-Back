86daef45ad3825adb74fa5a1ad6a5e3b3cb9ad6e
Alexis Sanz <105322087+AlexisSanz999@users.noreply.github.com>
2023-03-21 20:50:38 -0300

    Develop (#11)
    * login, registro y crud
    
    * login, registro y crud
    
    * login y registro listo
    
    * trayendo
    
    trayendo rama de shorsh
    
    * modularizado y rutas del auth listas
    
    * commit
    
    * intentando deploy
    
    * intentando deploy 2
    
    * listo deploy
    
    ---------
    
    Co-authored-by: jorrgett <jorgerafaeltorrestoro@gmail.com>
    Co-authored-by: VickyDuarte1 <victoria.durte@gmail.com>

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from auth_routes import auth
+from doctor_routes import doctor
+from patient_routes import patient
+
+app = Flask(__name__)
+CORS(app)
+
+# Ruta de autenticación
+app.register_blueprint(auth)
+
+# Ruta CRUD doctores
+app.register_blueprint(doctor)
+
+# Ruta CRUD pacientes
+app.register_blueprint(patient)
+
+if __name__ == '__main__':
+    app.run(debug=True)
