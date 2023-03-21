58d84596cb42db67b09d1e39a4275d0c87d510fb
jorrgett <jorgerafaeltorrestoro@gmail.com>
2023-03-20 20:53:30 -0400

    from flask_cors import CORS

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from auth_routes import auth
 from doctor_routes import doctor
 from patient_routes import patient
 
 app = Flask(__name__)
+CORS(app)
 
 # Ruta de autenticación
 app.register_blueprint(auth)
