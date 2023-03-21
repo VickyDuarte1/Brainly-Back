f3cd597c4771823f298070a8b5b8550b92dce8a6
Alexis Sanz <105322087+AlexisSanz999@users.noreply.github.com>
2023-03-21 19:56:01 -0300

    intentando deploy 2

--- api/app.py
+++ api/app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 from auth_routes import auth
-from crud_routes import crud
+from doctor_routes import doctor
+from patient_routes import patient
 
 app = Flask(__name__)
+CORS(app)
 
-# Rutas de autenticación
+# Ruta de autenticación
 app.register_blueprint(auth)
-app.register_blueprint(crud)
+
+# Ruta CRUD doctores
+app.register_blueprint(doctor)
+
+# Ruta CRUD pacientes
+app.register_blueprint(patient)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
