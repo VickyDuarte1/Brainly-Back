5d20c204107febfb10ff2f531c7fa6232cb5bdcc
CaroRuvira <caroruvira@gmail.com>
2023-03-27 11:57:45 -0300

    Back del main

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask_cors import CORS
 from auth_routes import auth
 from doctor_routes import doctor
 from patient_routes import patient
-<<<<<<< HEAD
 from cloudinary_routes import cloud
-=======
 from mp_routes import merpago
->>>>>>> main
 
 app = Flask(__name__)
 CORS(app)
@@ -21,14 +18,11 @@ app.register_blueprint(doctor)
 # Ruta CRUD pacientes
 app.register_blueprint(patient)
 
-<<<<<<< HEAD
 #Ruta cloudinary
 
 app.register_blueprint(cloud)
-=======
 # Ruta MERCADO_PAGO 
 app.register_blueprint(merpago)
->>>>>>> main
 
 if __name__ == '__main__':
     app.run()
