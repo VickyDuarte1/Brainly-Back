c950a69668604df8c8b3da4d180a7d4650e18808
VickyDuarte1 <112184349+VickyDuarte1@users.noreply.github.com>
2023-04-02 19:43:41 -0300

    Update patient_routes.py

--- routes/patient_routes.py
+++ routes/patient_routes.py
@@ -5,7 +5,7 @@ import sqlite3
 import os
 
 patient = Blueprint('patient', __name__)
-socketio = SocketIO()
+socketio = SocketIO(app)
 
 # Obtener la ruta base de tu proyecto
 basedir = os.path.abspath(os.path.dirname(__file__))
