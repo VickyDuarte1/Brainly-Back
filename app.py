cdbbb281812f2f5c067c806bbc936df6f12d3d1d
VickyDuarte1 <112184349+VickyDuarte1@users.noreply.github.com>
2023-04-02 19:56:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_cors import CORS
-from socketio import socketio
 from routes.auth_routes import auth
 from routes.doctor_routes import doctor
 from routes.patient_routes import patient
@@ -9,7 +8,6 @@ from routes.comments_route import comments
 
 
 app = Flask(__name__)
-socketio.init_app(app)
 CORS(app)
 
 # Ruta de autenticación
@@ -27,8 +25,6 @@ app.register_blueprint(merpago)
 # Ruta Comentarios/Reviews
 app.register_blueprint(comments)
 
-socketio = SocketIO(app)
 
 if __name__ == '__main__':
-    socketio.run(app)
     app.run()
