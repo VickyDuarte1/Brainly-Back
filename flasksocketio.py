4db4eb4a2e979287af957a8e8aa26235e3583f95
VickyDuarte1 <112184349+VickyDuarte1@users.noreply.github.com>
2023-04-03 01:14:04 -0300

    Update flasksocketio.py

--- flasksocketio.py
+++ flasksocketio.py
@@ -1,11 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
-from routes.auth_routes import auth
-from routes.doctor_routes import doctor
-from routes.patient_routes import patient
-from routes.mp_routes import merpago
-from routes.comments_route import comments
 
 app = Flask(__name__)
 CORS(app)
